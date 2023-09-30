8d024e94a2967c9ad4508af7b15e39e65fb311ed
Prisca <prisca.castanier@gmail.com>
2023-09-30 03:02:57 +0200

    change class naming for fade in up and change global font

--- src/components/Animations/FadeInUpAnimation.tsx
+++ src/components/Animations/FadeInUpAnimation.tsx
@@ -11,21 +11,20 @@ const FadeInUpAnimation = ({
   time = 1000,
   classKey,
 }: FadeInUpAnimationProps) => {
-  const customKey = 'fade-in-animation-for--' + classKey;
+  const customKey = 'fade-in-up-animation-for--' + classKey;
 
   useEffect(() => {
     const timer = setTimeout(() => {
       const elements = document.querySelectorAll(`.${customKey}`);
       elements.forEach((element) => {
-        element.classList.add('fade-in-animation--active');
+        element.classList.add('fade-in-up-animation--active');
       });
     }, time);
-    console.log('fade in up animation');
 
     return () => clearTimeout(timer);
-  }, [time]);
+  }, [customKey, time]);
 
-  return <div className={'fade-in-animation ' + customKey}>{children}</div>;
+  return <div className={'fade-in-up-animation ' + customKey}>{children}</div>;
 };
 
 export default FadeInUpAnimation;
