d750195d3aba7212894d970f8a32451bc30120c8
Prisca <prisca.castanier@gmail.com>
2023-09-30 16:11:29 +0200

    create a mystack component and hide scrollbar

---
+++ src/components/MyStack/MyStack.tsx
@@ -0,0 +1,5 @@
+const MyStack = () => {
+  return <div>MyStack</div>;
+};
+
+export default MyStack;
--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,9 +1,11 @@
 import Header from '../components/Header/Header.tsx';
+import MyStack from '../components/MyStack/MyStack.tsx';
 
 const LandingPage = () => {
   return (
     <div className={'landing-page'}>
       <Header />
+      <MyStack />
     </div>
   );
 };
