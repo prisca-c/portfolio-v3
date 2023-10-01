3c9f8d468c332f38cdefd1f6d8c5e0038566a587
Prisca <prisca.castanier@gmail.com>
2023-10-01 20:27:33 +0200

    add css tools to tech list

--- src/components/MyStack/MyStack.tsx
+++ src/components/MyStack/MyStack.tsx
@@ -1,20 +1,22 @@
 import techList from '../../datas/tech_list.json';
 import TechItem from './TechItem.tsx';
 const MyStack = () => {
-  const { front, back, languages } = techList;
+  const { front, back, languages, css } = techList;
   const title = {
     front: 'Frontend',
     back: 'Backend',
     languages: 'Languages',
+    css: 'CSS',
   };
 
   return (
     <div className={'my-stack'}>
-      <h2 className={'my-stack__title'}>I work with</h2>
+      <h2 className={'my-stack__title'}>My toolbox</h2>
       <div className={'my-stack__container'}>
         <TechItem title={title.front} techs={front} />
         <TechItem title={title.back} techs={back} />
         <TechItem title={title.languages} techs={languages} />
+        <TechItem title={title.css} techs={css} />
       </div>
     </div>
   );
