30b4c0db26a6c591e42d92c34ec23dd1bc7674bd
Prisca <prisca.castanier@gmail.com>
2023-10-01 02:41:32 +0200

    feat(tech): add techiItem to MyStack component and add css style to items

--- src/components/MyStack/MyStack.tsx
+++ src/components/MyStack/MyStack.tsx
@@ -1,5 +1,22 @@
+import techList from '../../datas/tech_list.json';
+import TechItem from './TechItem.tsx';
 const MyStack = () => {
-  return <div className={'my-stack'}>MyStack</div>;
+  const { front, back, languages } = techList;
+  const title = {
+    front: 'Frontend',
+    back: 'Backend',
+    languages: 'Languages',
+  };
+
+  return (
+    <div className={'my-stack'}>
+      <div className={'my-stack__container'}>
+        <TechItem title={title.front} techs={front} />
+        <TechItem title={title.back} techs={back} />
+        <TechItem title={title.languages} techs={languages} />
+      </div>
+    </div>
+  );
 };
 
 export default MyStack;
